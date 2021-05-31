d0b10198db036ec949cd1657ddd6aaac1e7ff295
Edgar Aroutiounian <edgar.factorial@gmail.com>
2021-05-31 14:30:53 -0400

    init cut

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cheap-name
+
+go 1.16
+
+require github.com/ethereum/go-ethereum v1.10.3
---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+var (
+	signature      = flag.String("selector", "", "selector given")
+	found     bool = false
+)
+
+// Perm calls f with each permutation of a.
+func Perm(a []rune, f func([]rune)) {
+	perm(a, f, 0)
+}
+
+// Permute the values at index i to len(a)-1.
+func perm(a []rune, f func([]rune), i int) {
+	if i > len(a) {
+		f(a)
+		return
+	}
+	if found {
+		return
+	}
+	perm(a, f, i+1)
+	for j := i + 1; j < len(a); j++ {
+		a[i], a[j] = a[j], a[i]
+		perm(a, f, i+1)
+		a[i], a[j] = a[j], a[i]
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *signature == "" {
+		log.Fatal("no selector given")
+	}
+
+	sig := *signature
+
+	atMost := []byte{0x00, 0x00, 0x00, 0xff}
+	wanted := []byte{0x00, 0x00, 0x00, 0x01}
+	started := time.Now()
+
+	Perm([]rune("abcdefghijklmn"), func(a []rune) {
+		combined := string(a) + "(" + sig + ")"
+		b := crypto.Keccak256([]byte(combined))[:4]
+
+		if bytes.Equal(wanted, b) {
+			fmt.Println("FOUND after", time.Since(started), "signature should be", combined)
+			return
+		}
+
+		if bytes.Compare(b, atMost) == -1 {
+			fmt.Println("this is good enough - can do ctrl-c now",
+				"use this as your signature",
+				combined, "found after",
+				time.Since(started),
+				hexutil.Encode(b),
+			)
+
+			found = true
+			return
+		}
+	})
+}
